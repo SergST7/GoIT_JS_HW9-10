70c5c2829c66ebacc5aee40cca9e63672c00dc44
Sergey Stelmakh <stelmakhserg@gmail.com>
2016-09-05 22:40:28 +0300

    add dropdown menu

--- js/main.js
+++ js/main.js
@@ -19,33 +19,38 @@ $(function () {
   });
 
   $('.jcarousel-pagination').jcarouselPagination({
+    'perPage': 1,
     item: function (page) {
       return '<a href="#' + page + '">' + page + '</a>';
     }
-
+  }).on('jcarouselpagination:active', 'a', function () {
+    $(this).addClass('active');
+  }).on('jcarouselpagination:inactive', 'a', function () {
+        $(this).removeClass('active');
   });
+ 
 
   // custom select
   $(".chosen-select").chosen({
     allow_single_deselect: true
   });
 
-  $(".chosen-select-width").chosen({width: "20%"});
+  $(".chosen-select-width").chosen({width: "30%"});
 
   // custom checkbox
   // проверяем, какие чекбоксы активны и меняем класс для родительского дива
-  $('.decor_checkbox').each(function(){
+  $('.decor_checkbox').each(function () {
     var checkbox = $(this).find('input[type=checkbox]');
-    if(checkbox.prop("checked")){
-      if(checkbox.prop("disabled"))  $(this).addClass("check_active-dis");
+    if (checkbox.prop("checked")) {
+      if (checkbox.prop("disabled"))  $(this).addClass("check_active-dis");
       $(this).addClass("check_active");
-    }else if (checkbox.prop("disabled"))  $(this).addClass("no-check-dis");
+    } else if (checkbox.prop("disabled"))  $(this).addClass("no-check-dis");
   });
 
-  // при клике по диву, делаем проверку
-  $('.decor_checkbox label').click(function(e){
+  // при клике по лейбе, делаем проверку
+  $('.decor_checkbox label').click(function (e) {
     var checkbox = $(this).parent().find('input[type=checkbox]');
-    if(!checkbox.prop("disabled")) {
+    if (!checkbox.prop("disabled")) {
       // если чекбокс был активен
       if (checkbox.prop("checked")) {
         // снимаем класс с родительского дива
@@ -61,5 +66,37 @@ $(function () {
       }
     }
   });
+
+  // drop-down menu
+  $('.menu-horizontal').find('ul').css({'display': 'none'});
+
+  $('.menu-horizontal > li').hover(
+      function () {
+        $(this).addClass("active");
+        $(this).find('ul').stop(true, true); // останавливаем всю текущую анимацию
+        $(this).find('ul:first').slideDown();
+      },
+      function () {
+        $(this).removeClass("active");
+        $(this).find('ul:first').slideUp('fast');
+      }
+  );
+
+  var subMenu = $('.menu-horizontal ul li');
+
+  subMenu.find('ul').css({'display': 'none'});
+
+  subMenu.hover(
+      function () {
+        $(this).find('ul:first').stop(true, true);
+        $(this).find('ul:first').fadeIn();
+      },
+      function () {
+        $(this).find('ul:first').fadeOut('fast');
+      }
+  );
+  $('.menu-horizontal ul li:has(ul)').find('a:first').addClass('sub-menu');
+  $('.menu-horizontal > li:has(ul)').find('a:first').addClass('drop-menu');
 });
 
+ 
